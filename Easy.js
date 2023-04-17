6b168c23eadc6ca267059eb324a782392b24ea90
OAKevin <kevinmirandat@gmail.com>
2023-04-17 17:33:58 -0400

    finished Easy.js

---
+++ Easy.js
@@ -0,0 +1,11 @@
+var name1 = "Kevin"
+var name2 = "Romeo Santos"
+
+if (name1.length > name2.length) {
+    let remainder = name1.length - name2.length 
+    console.log("The name "+ name1 + " is longer than "+name2+ " by "+ remainder +" characters.")
+}else {
+    let remainder = name2.length - name1.length 
+    console.log("The name "+ name2 + " is longer than "+name1+ " by "+ remainder +" characters.")
+}
+
