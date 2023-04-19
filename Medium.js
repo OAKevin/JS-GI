9b23e1bdc9f066cc8a692fd135ff607fcf9e7403
OAKevin <kevinmirandat@gmail.com>
2023-04-19 00:50:41 -0400

    Finished Very Easy, Easy and Medium

---
+++ Medium.js
@@ -0,0 +1,8 @@
+var input = window.prompt("Type something");
+
+if (input === input.toUpperCase()) {document.write ("The user is screaming");
+}else if(input === input.toLowerCase()){document.write ("The user is whispering");
+}
+else{(document.write("The user is doing neither"))
+
+}
